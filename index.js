dda3ba45e1d47310e778d81ff2f0c776d542887f
Victoria LeBel <vickileb@hotmail.com>
2022-05-11 10:41:18 -0700

    commit tree 1bc17d6f00e3ffcfde5e82a6a0b559fc2d2421c1

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solutions in this file
+/* const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i=0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+function writeCards(names, eventName) {
+    const eventArr = []
+    for (let i= 0; i < names.length; i++) {
+        eventArr.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }  
+    return eventArr;
+}
+
+writeCards(["Guadalupe", "Ollie", "Alki"], "surprise");
+
+function countDown(count) {
+    let x= 10;
+    while (x <= 10) {
+        if (x < 0){
+            break;
+        }
+        console.log(x--);
+    }   
+}
